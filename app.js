4c10de0b9162267e2a7c8d98e350130347377dd5
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2023-05-19 19:21:00 +0300

    testing

---
+++ app.js
@@ -0,0 +1,26 @@
+require('./bootstrap');
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { i18nVue } from 'laravel-vue-i18n'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+
+
+createApp(App).use(vuetify).use(i18nVue,{
+  lang: 'ar',
+    resolve: lang => import(`./lang/${lang}.json`),
+}).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
