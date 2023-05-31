41ad4807a7551403a3f2acc8301dc70fbc6c56a3
Mostafa Zayed <61474987+Mostafa-Zayed@users.noreply.github.com>
2023-05-31 19:33:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ require('./bootstrap');
 // import * as Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import axios from 'axios';
+import VueAxios from 'vue-axios'
+import VueSocialauth from 'vue-social-auth'
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
